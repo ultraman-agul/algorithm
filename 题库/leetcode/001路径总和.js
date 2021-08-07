3b407037fbe6186fa2e85aa6b8cae4c033323d13
ultraman-agul <agulagul@163.com>
2021-08-07 17:38:05 +0800

    练

---
+++ 题库/leetcode/001路径总和.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+    if (!root) { return false }
+    var cur = targetSum - root.val
+    if (!root.left && !root.right) { return cur === 0 }
+    return hasPathSum(root.left, cur) || hasPathSum(root.right, cur)
+};
